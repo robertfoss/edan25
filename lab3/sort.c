f1aa96325db167bebebca331894dcb60c95cd188
Robert Foss <dev@robertfoss.se>
2012-04-16 16:42:10 +0200

    Exploring a threaded merge sort.

--- lab3/sort.c
+++ lab3/sort.c
@@ -3,13 +3,114 @@
 #include <pthread.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <sys/times.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <math.h>
 
-typedef struct timeval tv;
+#define NTHREADS 4
 
+
+inline
+unsigned int find_recursion_depth(unsigned int x)
+{
+	
+	return (unsigned int) (ceil (log( (double) x ) / log( 2.0 ))   );
+}
+
+ 
+inline
+void merge(double *left, int l_len, double *right, int r_len, double *out, int (*cmp)(const void *, const void *))
+{
+	int i, j, k;
+	for (i = j = k = 0; i < l_len && j < r_len; )
+		out[k++] = left[i] < right[j] ? left[i++] : right[j++];
+ 
+	while (i < l_len) out[k++] = left[i++];
+	while (j < r_len) out[k++] = right[j++];
+}
+
+struct recur_struct{
+	double *buf;
+	double *tmp; 
+	int len;
+	unsigned int recursion_depth;
+	unsigned int max_thread_split_depth;
+	int (*cmp)(const void *, const void *);
+};
+typedef struct recur_struct recur_struct;
+
+/* inner recursion of merge sort */
+void recur(void* rs_in)
+{
+	recur_struct rs = *((recur_struct*) rs_in);
+	
+	int l = rs.len / 2;
+	if (rs.len <= 1) return;
+ 	
+ 	pthread_t thread;
+	int status = -111;
+		
+	recur_struct rs_new;
+	rs_new.buf 						= rs.tmp +l;
+	rs_new.tmp 						= rs.buf + l;
+	rs_new.len 						= rs.len - l;
+	rs_new.recursion_depth 			= rs.recursion_depth + 1;
+	rs_new.max_thread_split_depth 	= rs.max_thread_split_depth;
+	rs_new.cmp						= rs.cmp;
+	
+	if (rs.recursion_depth <= rs.max_thread_split_depth){
+		/*
+		status = pthread_create( &thread, NULL, recur, &rs_new);
+		if (status != 0){
+			printf("Horrible error occured, thread couldn't be created!\nAborting..\n");
+			exit(1);
+		}*/
+		//recur(tmp + l, buf + l, len - l, ++recursion_depth, max_thread_split_depth, cmp); //Splitsings!
+	} else {
+		//recur(tmp + l, buf + l, len - l, ++recursion_depth, max_thread_split_depth, cmp);	
+	}
+		recur( (void*) &rs_new);
+	
+	
+	double* tmp_var = rs.tmp;	
+	rs.tmp = rs.buf;
+	rs.buf = tmp_var;
+	rs.len = l;
+	rs.recursion_depth = rs.recursion_depth + 1;
+	//rs.max_thread_split_depth;
+	//rs.cmp;
+	recur((void*) &rs);
+	
+	
+	if (status != -111){
+		pthread_join(thread, NULL); // Wait here
+	}
+ 
+	merge(rs.tmp, l, rs.tmp + l, rs.len - l, rs.buf, rs.cmp);
+}
+ 
+/* preparation work before recursion */
+void merge_sort(double *a, size_t len, size_t elem_size, int (*cmp)(const void *, const void *))
+{
+	/* call alloc, copy and free only once */
+	double *tmp = malloc(elem_size * len);
+	memcpy(tmp, a, elem_size * len);
+ 
+ 	recur_struct rs;
+ 	rs.buf = a;
+ 	rs.tmp = tmp;
+ 	rs.len = len;
+ 	rs.recursion_depth = 0;
+ 	rs.max_thread_split_depth = find_recursion_depth( NTHREADS);
+ 	rs.cmp = cmp;
+ 	
+	recur( (void*) &rs );
+ 
+	free(tmp);
+}
 
 
 static double sec(void)
@@ -62,6 +163,26 @@ int main(int ac, char** av)
 	printf("%1.2f s\n", end - start);
 
 	free(a);
+	
+	
+	#	define LEN 20
+	double x[LEN];
+ 
+	for (i = 0; i < LEN; i++)
+		x[i] = rand() % LEN;
+ 
+	puts("before sort:");
+	for (i = 0; i < LEN; i++) printf("%1.0f ", x[i]);
+	putchar('\n');
+ 
+	merge_sort(x, LEN, sizeof(x[0]), cmp);
+ 
+	puts("after sort:");
+	for (i = 0; i < LEN; i++) printf("%1.0f ", x[i]);
+	putchar('\n');
+ 	
+ 	printf("round power-2 %d\n", find_recursion_depth(5));
+
 
 	return 0;
 }
