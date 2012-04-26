8f401101d90b4b955a4589c0cf4875016efdc633
Angelica Gabasio <dt08ag8@student.lth.se>
2012-04-26 09:26:09 +0200

    changes on bitset_or

--- lab4/bitset.c
+++ lab4/bitset.c
@@ -45,6 +45,7 @@ void bitset_or(BitSet_struct* result, BitSet_struct* arg){
     unsigned int arg_offset = arg_bss->offset;
 
     do{
+        printf("ro: %u\tao: %u\n", result_offset, arg_offset);
         if(arg_offset == result_offset){
             result_bss->bit = or_bits(result_bss->bit, arg_bss->bit);
             result_offset = result_bss->offset;
@@ -57,8 +58,18 @@ void bitset_or(BitSet_struct* result, BitSet_struct* arg){
             result_l = result_l->next; //result_l == next in original result_l
             result_bss = result_l->data;
             result_offset = result_bss->offset;
+            printf("ro inside #1: %u\n", result_offset);
 
         }else{ //arg_offset < result_offset
+            BitSetSubset_struct* new_bss = bitsetsubset_create(arg_offset);
+            new_bss->bit = arg_bss->bit;
+            insert_before(result_l, create_node(new_bss));
+           // result_l = result_l->next; //result_l == new_bss node
+           // result_l = result_l->next; //result_l == next in original result_l
+            //result_bss = result_l->data;
+            //result_offset = result_bss->offset;
+            printf("ro inside #2: %u\n", result_offset);
+
             arg_l = arg_l->next;
             arg_bss = arg_l->data;
             arg_offset = arg_bss->offset;
@@ -67,7 +78,6 @@ void bitset_or(BitSet_struct* result, BitSet_struct* arg){
     }while(arg_l->next != arg_l && result_l->next != result_l);
 
     while(arg_l->next != arg_l){ //reached end of result_l but not arg_l
-        arg_l = arg_l->next;
         arg_bss = arg_l->data;
         arg_offset = arg_bss->offset;
 
@@ -76,6 +86,8 @@ void bitset_or(BitSet_struct* result, BitSet_struct* arg){
 
         insert_after(result_l, create_node(new_bss));
         result_l = result_l->next; //result_l == new_bss node (new last in result_l)
+
+        arg_l = arg_l->next;
     }
 }
 
--- lab4/bitset_test.c
+++ lab4/bitset_test.c
@@ -32,7 +32,26 @@ void test_bitset_or(){
 
     bss2 = bitset_create();
 
-    printf("******\n");
+    printf("*********************************\n");
+    bitset_print(bss1);
+    printf("or\n");
+    bitset_print(bss2);
+    printf("=\n");
+    
+    bitset_or(bss2, bss1);
+    bitset_print(bss1);
+
+/////////////////
+
+    bss1 = bitset_create();
+    bitset_set_bit(bss1, 55, true);
+
+    bss2 = bitset_create();
+    bitset_set_bit(bss2, 1, true);
+    bitset_set_bit(bss2, 3, true);
+    bitset_set_bit(bss2, 61, true);
+
+    printf("*********************************\n");
     bitset_print(bss1);
     printf("or\n");
     bitset_print(bss2);
@@ -136,10 +155,10 @@ void test_bitset_copy(){
 int main(){
     test_bitset_or();
     //test_bitset_and_not();
-    test_bitset_equals();
-    test_bitset_set_bit();
-    test_bitset_get_bit();
-    test_bitset_copy();
+    //test_bitset_equals();
+    //test_bitset_set_bit();
+    //test_bitset_get_bit();
+    //test_bitset_copy();
 }
 
 
